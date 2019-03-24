8ec763d3530963754c9fabf13af231c975b384d8
Alexandra_Kelle <alexandra_kelle@MacBook-Air-Alexandra.local>
2019-03-24 18:25:03 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = new Set();
+  const closeBrackets = new Set();
+  const bracketsPairs = new Set();
+
+  bracketsConfig.forEach(config => {
+    openBrackets.add(config[0]);
+    closeBrackets.add(config[1]);
+    bracketsPairs.add(`${config[0]}${config[1]}`)
+  });
+
+  const bracketsState = [];
+
+  for(let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    if (openBrackets.has(bracket)) {
+      if (closeBrackets.has(bracket)) {
+        const closeBracket = bracketsState[bracketsState.length - 1];
+        if (closeBracket === bracket) {
+          bracketsState.pop();
+        } else {
+          bracketsState.push(bracket);  
+        }
+      } else {
+        bracketsState.push(bracket);
+      }
+    } else if (closeBrackets.has(bracket)) {
+      if (bracketsState.length) {
+        const closeBracket = bracketsState.pop();
+        if (!bracketsPairs.has(`${closeBracket}${bracket}`)) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if (bracketsState.length) {
+    return false;
+  }
+
+  return true;
 }
